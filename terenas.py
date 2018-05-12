774c10fbff8284e527bae905c20f0b7961a1eb20
Sergey Bespalov <sergey.bespalov@chisw.us>
2018-05-12 15:46:13 +0300

    fix response

--- terenas.py
+++ terenas.py
@@ -21,26 +21,25 @@ def check(code):
 @app.route(token, methods=['POST'])
 def bot_handler():
     content = request.get_json()
-    return Response(content)
-    # command = content['message']['text']
-    # url = 'https://api.telegram.org/bot' + bot + '/sendMessage'
-    # if command == '/hello':
-    #     text = 'Hello, I`m working now.'
-    # elif command == '/start':
-    #     return Response(1)
-    # elif command == '/birthdays':
-    #     text = 'The list of Birthdays.'
-    # elif command == '/greet':
-    #     text = 'Greet El Presidente!!!'
-    # elif command == '/news':
-    #     text = 'News news news.'
-    # elif command == '/joke':
-    #     text = 'Jokes and funny stories'
-    # else:
-    #     return Response(1)
-    # data = {'chat_id': chat_id, 'text': text, 'disable_notification': 1}
-    # requests.post(url, data)
-    # return Response(1)
+    command = content['message']['text']
+    url = 'https://api.telegram.org/bot' + bot + '/sendMessage'
+    if command == '/hello':
+        text = 'Hello, I`m working now.'
+    elif command == '/start':
+        return Response(str(1))
+    elif command == '/birthdays':
+        text = 'The list of Birthdays.'
+    elif command == '/greet':
+        text = 'Greet El Presidente!!!'
+    elif command == '/news':
+        text = 'News news news.'
+    elif command == '/joke':
+        text = 'Jokes and funny stories'
+    else:
+        return Response(str(1))
+    data = {'chat_id': chat_id, 'text': text, 'disable_notification': 1}
+    requests.post(url, data)
+    return Response(str(1))
 
 
 if __name__ == '__main__':
