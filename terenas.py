02bb93d0f3391e2f27365f29855a87478f1603a0
Sergey Bespalov <sergey.bespalov@chisw.us>
2018-05-12 18:03:39 +0300

    added jokes

--- terenas.py
+++ terenas.py
@@ -1,3 +1,6 @@
+import os
+import random
+
 from flask import Flask, request
 from werkzeug.wrappers import Response
 from settings import token, bot, chat_id
@@ -34,10 +37,14 @@ def bot_handler():
     elif command == '/news':
         text = 'News news news.'
     elif command == '/joke':
-        text = 'Jokes and funny stories'
+        with open(os.path.dirname(os.path.abspath(__file__)) + '\jokes.txt', 'r') as myfile:
+            data = myfile.read().split('#')
+        num_string = random.randint(0, 10)
+        text = data[num_string]
     else:
         return Response(str(2))
     data = {'chat_id': chat_id, 'text': text, 'disable_notification': 1}
+    print data
     requests.post(url, data)
     return Response(str(3))
 
